ddce7ba905c984c7ac97010216c2f0bb29f94cae
Tyler Mays <richard.tyler.mays@gmail.com>
2015-12-30 13:46:09 -0800

    List books with rating filter

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -3,15 +3,24 @@ require 'test_helper'
 class ListingBooksTest < ActionDispatch::IntegrationTest
   def setup
     Book.create!(title: 'Pragmatic Programmer', rating: 5)
-    Book.create!(title: "Ender's Game",         rating: 5)
+    Book.create!(title: "Ender's Game",         rating: 4)
   end
 
   test 'lisiting books' do
-     get '/books'
+    get '/books'
 
-     assert_response :success
-     assert_equal Mime::JSON, response.content_type
+    assert_response :success
+    assert_equal Mime::JSON, response.content_type
 
-     assert_equal Book.count, JSON.parse(response.body).size
+    assert_equal Book.count, JSON.parse(response.body).size
+  end
+
+  test 'lists top rated books' do
+    get '/books?rating=5'
+
+    assert_response :success
+    assert_equal Mime::JSON, response.content_type
+
+    assert_equal 1, JSON.parse(response.body).size
   end
 end
